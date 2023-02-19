4f30d6de9595f08245dc22f01e3826cbf0c3e72b
黃泓量 Jeremy <jmw@netvigator.com>
2023-02-20 00:47:40 +0800

    [New] nExample???

--- bigint_literal.cpp
+++ bigint_literal.cpp
@@ -91,7 +91,10 @@ int main()
 
     static_assert((int)15_bigint == 15, "ha");
 
-    int nA = 1_bigint;
+//    __int128  nExample128 = -0x80000000000000000000000000000000_bigint;
+    long long nExample64 = -0x8000000000000000_bigint;
+    int       nExample32 = -0x80000000_bigint;
+    short     nExample16 = -0x8000_bigint;
 
     std::cout << "Hello World!\n";
 }
